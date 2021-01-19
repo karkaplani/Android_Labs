eb5d90b2bf57be3b4ea82e93719e7ee1b719a481
karkaplani <ilgu0001@algonquinlive.com>
2021-01-18 20:28:33 -0500

    initial commit

---
+++ app/src/main/java/com/cst2355/ilgu0001/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cst2355.ilgu0001
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
