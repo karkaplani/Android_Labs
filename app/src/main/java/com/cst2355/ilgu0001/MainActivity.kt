087333e98191a3a08859d978fce6de39213b667c
karkaplani <ilgu0001@algonquinlive.com>
2021-02-01 17:09:08 -0500

    step 1

--- app/src/main/java/com/cst2355/ilgu0001/MainActivity.kt
+++ app/src/main/java/com/cst2355/ilgu0001/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.cst2355.ilgu0001
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.View
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.cst2355.ilgu0001.R.layout.activity_main_linear
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main_linear)
+
+        val myButton: Button = findViewById<Button>(R.id.myButton)
+        val mySwitch : Switch = findViewById<Switch>(R.id.mySwitch)
+
+        val toastText: String = getString(R.string.ToastText)
+        val switchText: String = getString(R.string.SnackText)
+        val switchOn: String = getString(R.string.SwitchOn)
+        val switchOff: String = getString(R.string.SwitchOff)
+
+        myButton.setOnClickListener {
+            Toast.makeText(applicationContext, toastText, Toast.LENGTH_LONG).show()
+        }
+
+        mySwitch.setOnCheckedChangeListener { _, isChecked ->
+            var b = false
+
+            var state = switchOff
+            if (isChecked) {
+                state = switchOn
+                b = true
+            } else if (!isChecked) {
+                state = switchOff
+                b = false
+            }
+            var snackbar = Snackbar.make(mySwitch, "$switchText $state", Snackbar.LENGTH_LONG).show()
+        }
     }
 }
